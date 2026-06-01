a9b957a38cee7190350847cd4339da5a36430134
neclor <neclordev@gmail.com>
2026-06-01 10:01:01 +0200

    test

--- server/NetrisServer/Leaderboard/LeaderboardOptions.cs
+++ server/NetrisServer/Leaderboard/LeaderboardOptions.cs
@@ -5,6 +5,6 @@ internal sealed class LeaderboardOptions {
 #pragma warning restore CA1812
 	public const string SectionName = "Leaderboard";
 
-	public string FilePath { get; init; } = "netris_leaderboard.csv";
+	public string FilePath { get; init; } = "leaderboard.csv";
 	public int MaxEntries { get; init; } = 200;
 }
